d41822f1d479536c8411828d80da87981ec920c1
asifrana86 <134046698+asifrana86@users.noreply.github.com>
2023-05-27 18:27:27 +0500

    today work

--- tradex/src/App.js
+++ tradex/src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 // import SignupForm from './pages/singup';
 import Signup20 from './pages/signup2.0';
-
+import LoginForm from './pages/login-form';
 function App() {
   return (
     <div className="App">
      <Signup20/>
+     <LoginForm/>
     </div>
   );
 }
